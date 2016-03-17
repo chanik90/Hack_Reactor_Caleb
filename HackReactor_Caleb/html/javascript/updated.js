30d3f0ce799c512b6e60219f672d390f1ac9ea3e
ChanIk Jang <chanikjang90@gmail.com>
2016-03-17 15:04:58 -0700

    second_updates

---
+++ HackReactor_Caleb/html/javascript/updated.js
@@ -0,0 +1,14 @@
+
+
+
+var friends = ["Mike", "Stacy", "Andy", "Rick"];
+
+friends.forEach(function(eachName, index){
+	console.log(index + 1 + "." + eachName);
+}
+
+var myVar="hello";
+function showAlert() { alert('You triggered an alert!'); }
+showAlert(myVar);
+
+
